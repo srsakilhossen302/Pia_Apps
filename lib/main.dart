b9fc642331dfcd2f6782c6b3b3a26ce151ec82a5
srsakilhossen302 <cse.sakilhossem@gmail.com>
2026-03-08 23:34:19 +0600

    feat: Initialize Flutter project with 'ASCELA' branding, app icons, and core application setup.

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'Pia Apps',
+          title: 'ASCELA',
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
